06cf6a683cb298ec861dd12f71fc5af3bac4d913
raswant <raswantraswant@gmail.com>
2026-07-01 20:24:36 +0530

    added Linter Phase-1

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 from test_app import company
-def add(x,y):
+
+
+def add(x, y):
     return x + y
 
-print("From app.py buddy:",add(2, 3))
+
+print("From app.py buddy:", add(2, 3))
 
 
-def div(x1,y1):
-    if y1<=0:
+def div(x1, y1):
+    if y1 <= 0:
         raise ValueError("Denominators must be greater than zero")
     return x1 / y1
 
 
-class Company: 
-    def __init__(self): 
+class Company:
+    def __init__(self):
         self.employees = {}
-    def add_employee(self, employee,name):
+
+    def add_employee(self, employee, name):
         if employee in self.employees:
             raise ValueError("Employee with this ID already exists")
         self.employees[employee] = name
-        
+
     def get_employee(self, employee_id):
         if employee_id not in self.employees:
             raise ValueError("Employee with this ID does not exist")
-        return self.employees[employee_id] 
-    
+        return self.employees[employee_id]
+
     def delete_employee(self, employee_id):
         if employee_id not in self.employees:
             raise ValueError("Employee with this ID does not exist")
         del self.employees[employee_id]
-        
+
+
 print(company().employees)
 print("Completed app.py")
